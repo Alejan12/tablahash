2cc4b86fc635a9d2900b01b395cf0e31a05f6a9d
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2022-10-23 12:45:28 +0200

    subir nuevo

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,14 @@
-from 
+from turtle import position
+from primeraparte import crear_tabla,añadir,encontrar
+tabla=crear_tabla(9)
+nombre=input()
+while(nombre!=""):
+    añadir(tabla,nombre)
+    nombre=input()
+print("Nombre a buscar")
+buscado=input()
+posicion=encontrar(tabla,buscado)
+if posicion is not None:
+    print("elemento encontrado ", posicion.info)
+else:
+    print("No se ha encontrado na")
