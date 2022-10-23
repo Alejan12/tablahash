8a1f5c912384fa31d0a50c494881dd14baa4bf9c
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2022-10-23 12:36:38 +0200

    subir nuevo

---
+++ primeraparte.py
@@ -0,0 +1,37 @@
+from turtle import position
+
+
+def crear_tabla(tamaño):
+    tabla=[None]*tamaño
+    return tabla
+def numero_elementos(tabla):
+    return len(tabla)- tabla.count(None)
+def funcion_hash(dato, tamaño):
+    return len(str(dato).strip())%tamaño
+def añadir (tabla, dato):
+    lugar= funcion_hash(dato, len(tabla))
+    if tabla[lugar] is None:
+        tabla[lugar]=dato
+    else:
+        print("Algo va mal")
+def encontrar(tabla, buscar):
+    pos=None
+    lugar= funcion_hash(buscar, len(tabla))
+    if tabla[lugar] is not None:
+        if buscar== tabla[lugar]:
+            pos-lugar
+        else:
+            print("aplica funcion de sondeo")
+    return pos
+def eliminar(tabla, dato):
+    pos=None
+    lugar= funcion_hash(dato, len(tabla))
+    if tabla[lugar] is not None:
+        if dato== tabla[lugar]:
+            dato= tabla[lugar]
+            tabla[lugar]=None
+        else:
+            print("aplica funcion de sondeo")
+    return dato
+       
+   
